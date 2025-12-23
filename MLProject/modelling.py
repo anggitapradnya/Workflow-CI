e7a54cf97c7b1067341282a185aca7f197f03c49
anggitapradnya <anggitapradnya7@gmail.com>
2025-12-23 13:42:35 +0800

    Initial CI dengan MLflow Project

---
+++ MLProject/modelling.py
@@ -0,0 +1,46 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score, roc_auc_score
+import mlflow
+import mlflow.sklearn
+
+df = pd.read_csv("/telco_churn_preprocessing/telco_churn_clean.csv")
+
+X = df.drop(columns=['Churn'])
+y = df['Churn']
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42, stratify=y
+)
+
+mlflow.sklearn.autolog()
+
+model = RandomForestClassifier(
+    n_estimators=200,         
+    max_depth=10,             
+    min_samples_split=10,     
+    min_samples_leaf=5,       
+    max_features='sqrt',      
+    class_weight='balanced',  
+    random_state=42,
+    n_jobs=-1
+)
+
+with mlflow.start_run(run_name="RandomForest_Manual_Balanced"):
+    model.fit(X_train, y_train)
+
+    y_pred = model.predict(X_test)
+    y_proba = model.predict_proba(X_test)[:,1]  
+
+    acc = accuracy_score(y_test, y_pred)
+    f1 = f1_score(y_test, y_pred)
+    prec = precision_score(y_test, y_pred)
+    rec = recall_score(y_test, y_pred)
+    roc_auc = roc_auc_score(y_test, y_proba)
+
+    print(f"Accuracy: {acc:.4f}")
+    print(f"F1 Score: {f1:.4f}")
+    print(f"Precision: {prec:.4f}")
+    print(f"Recall: {rec:.4f}")
+    print(f"ROC AUC: {roc_auc:.4f}")
