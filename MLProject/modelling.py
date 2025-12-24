3f53a237372775a92c9c9d21a2f3f586f0bf4b80
anggitapradnya <anggitapradnya7@gmail.com>
2025-12-24 13:57:55 +0800

    update modelling

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,40 +1,53 @@
+import argparse
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score, roc_auc_score
 import mlflow
 import mlflow.sklearn
 
-df = pd.read_csv(
-    "MLProject/telco_churn_clean.csv"
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import (
+    accuracy_score,
+    f1_score,
+    precision_score,
+    recall_score,
+    roc_auc_score
 )
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--data_path",
+    type=str,
+    default="MLProject/telco_churn_clean.csv"
+)
+args = parser.parse_args()
+
+
+df = pd.read_csv(args.data_path)
 
 X = df.drop(columns=["Churn"])
 y = df["Churn"]
 
 X_train, X_test, y_train, y_test = train_test_split(
-    X,
-    y,
+    X, y,
     test_size=0.2,
     random_state=42,
     stratify=y
 )
 
-mlflow.set_experiment("Telco_Churn_RF")
+
+mlflow.set_experiment("Telco_Churn_CI")
+
 mlflow.sklearn.autolog()
 
-model = RandomForestClassifier(
-    n_estimators=200,
-    max_depth=10,
-    min_samples_split=10,
-    min_samples_leaf=5,
-    max_features="sqrt",
-    class_weight="balanced",
-    random_state=42,
-    n_jobs=-1
-)
+with mlflow.start_run(run_name="RF_CI_Run"):
 
-with mlflow.start_run(run_name="RF_Balanced_200trees"):
+    model = RandomForestClassifier(
+        n_estimators=200,
+        max_depth=10,
+        class_weight="balanced",
+        random_state=42
+    )
+
     model.fit(X_train, y_train)
 
     y_pred = model.predict(X_test)
@@ -44,12 +57,14 @@ with mlflow.start_run(run_name="RF_Balanced_200trees"):
     f1 = f1_score(y_test, y_pred)
     prec = precision_score(y_test, y_pred)
     rec = recall_score(y_test, y_pred)
-    roc_auc = roc_auc_score(y_test, y_proba)
+    roc = roc_auc_score(y_test, y_proba)
 
-    print(f"Accuracy : {acc:.4f}")
-    print(f"F1 Score : {f1:.4f}")
-    print(f"Precision: {prec:.4f}")
-    print(f"Recall   : {rec:.4f}")
-    print(f"ROC AUC  : {roc_auc:.4f}")
+    mlflow.log_metric("accuracy_manual", acc)
+    mlflow.log_metric("f1_manual", f1)
+    mlflow.log_metric("precision_manual", prec)
+    mlflow.log_metric("recall_manual", rec)
+    mlflow.log_metric("roc_auc_manual", roc)
 
-print("\nTraining selesai. Buka MLflow UI untuk melihat hasil.")
+    print("Training selesai")
+    print(f"Accuracy: {acc:.4f}")
+    print(f"F1 Score: {f1:.4f}")
